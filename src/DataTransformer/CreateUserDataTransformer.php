eb8e816b5b7cfcb1ac37fecfd01b2ad26507a183
gary houbre <ghoubre@sqli.com>
2021-06-24 13:40:07 +0200

    Init Project

---
+++ src/DataTransformer/CreateUserDataTransformer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataTransformer;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use ApiPlatform\Core\Serializer\AbstractItemNormalizer;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+final class CreateUserDataTransformer implements DataTransformerInterface
+{
+    private $securityEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $securityEncoder)
+    {
+        $this->securityEncoder = $securityEncoder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($data, string $to, array $context = [])
+    {
+        $user = new User();
+        $user->setEmail($data->email);
+        $user->setPassword($this->securityEncoder->encodePassword($user, $data->password));
+
+        return $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        if ($data instanceof User) {
+            return false;
+        }
+
+        return User::class === $to && null !== ($context['input']['class'] ?? null);
+    }
+}
---
+++ tests/AuthenticationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
+
+class AuthenticationTest extends ApiTestCase
+{
+    use ReloadDatabaseTrait;
+
+    public function testLogin(): void
+    {
+        $client = self::createClient();
+        $email = "test@test.fr";
+        $password = "$3CR3T";
+
+        $response = $client->request('POST', '/api/users', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => $email,
+                'password' => $password,
+            ],
+        ]);
+        $this->assertResponseIsSuccessful();
+
+        // retrieve a token
+        $response = $client->request('POST', '/authentication_token', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => $email,
+                'password' => $password,
+            ],
+        ]);
+
+        $json = $response->toArray();
+        $this->assertResponseIsSuccessful();
+        $this->assertArrayHasKey('token', $json);
+    }
+}
